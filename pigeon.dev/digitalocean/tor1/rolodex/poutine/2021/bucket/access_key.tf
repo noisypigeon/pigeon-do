f50105dd64835fe6dfd3ed94eed198c1eda26946
noisypigeon <167243387+noisypigeon@users.noreply.github.com>
2026-09-24 22:02:36 -0700

    chore(poutine): delete unused bucket

--- pigeon.dev/digitalocean/tor1/rolodex/poutine/2021/bucket/access_key.tf
+++
@@ -1,5 +0,0 @@
-module "rolodex_poutine_2021_key" {
-  source     = "git::https://github.com/noisypigeon/pigeon-tf.git//digitalocean/access-key?ref=digitalocean/access-key/v0.1.0"
-  name       = module.rolodex_poutine_2021.name
-  permission = "readwrite"
-}
